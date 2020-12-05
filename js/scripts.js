6d5f4ccd319434793ba7e58bcfd3170bf572ef06
David Jeyachandran <davidjeyachandran@gmail.com>
2020-12-05 12:54:43 -0500

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+ga = null;
+
+(function ($, ga) {
+  /* This code must NOT be in Drupal.behaviours because we call Drupal.attachBehaviors()
+    that calls all Drupal.behaviours and we do not want to call recursively the code below.
+  */
+
+  const externalScriptObject = new Object({
+    Drupal,
+    jQuery,
+    ga,
+  });
+
+  const websiteConfig = {
+    urlInclude: [],
+    urlExclude: [
+      "*logout*",
+      "/admin_menu*",
+      "/contact",
+      "*admin/*",
+      "*.txt",
+      "*.doc",
+      "*.docx",
+      "*.pdf",
+      "*.ppt",
+      "*.pptx",
+      "*.mp3",
+      "*.mp4",
+      "*.avi",
+      "*.jpg",
+      "*.jpeg",
+      "*.zip",
+    ],
+    doNotFetch: ["*logout*"],
+    elementSelector: null,
+    externalScriptObject: externalScriptObject,
+    url: {
+      "/": {
+        pageFunction: function (urlTarget, jQuery, ga) {},
+      },
+      all: {
+        pageFunction: function (urlTarget, externalScriptObject) {
+          const [Drupal, jQuery, ga] = externalScriptObject;
+          if (ga) {
+            ga("set", "dimension1", "faster");
+            ga("send", "pageview", urlTarget, {
+              location: window.location.href,
+            });
+          }
+          if (Drupal) Drupal.attachBehaviors();
+
+          console.log("All url callback: " + window.location.pathname);
+        },
+      },
+    },
+  };
+
+  window.addEventListener("load", function () {
+    console.log(websiteConfig);
+    // window.faster(websiteConfig, externalScriptObject);
+  });
+
+  Drupal.behaviors.fasterweb = {
+    attach: function (context, settings) {
+      console.log("Running Faster");
+    },
+  };
+})(jQuery, ga);
