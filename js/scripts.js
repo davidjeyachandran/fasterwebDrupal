4a75bc3da33a7384430460fd58787626151c7bde
David Jeyachandran <davidjeyachandran@gmail.com>
2021-01-24 18:27:29 -0500

    Add debug option and add to behaviors

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,46 @@
 (function ($, Drupal) {
-  /* This code must NOT be in Drupal.behaviours because we call Drupal.attachBehaviors()
-    that calls all Drupal.behaviours and we do not want to call recursively the code below.
-  */
+  Drupal.behaviors.fasterweb = {
+    attach: function (context, settings) {
+      // Ensure that we run this code only once
+      if (!Drupal.settings.fasterweb.isRunAlready) {
+        Drupal.settings.fasterweb.isRunAlready = true;
 
-  var externalScriptObject = new Object({
-    Drupal,
-    jQuery,
-  });
+        var externalScriptObject = new Object({
+          Drupal,
+          jQuery,
+        });
 
-  var websiteConfig = {
-    urlInclude: ["/education-center/*", "/knowledge-center/*"],
-    urlExclude: ["*logout*", "/admin_menu*", "*admin/*"],
-    doNotFetch: ["*logout*"],
-    elementSelector: null,
-    externalScriptObject: externalScriptObject,
-    url: {
-      "/": {
-        pageFunction: function (urlTarget, externalScriptObject) {},
-      },
-      all: {
-        pageFunction: function (urlTarget, externalScriptObject) {
-          console.log("All url callback: " + window.location.pathname);
+        var websiteConfig = {
+          debug: settings.fasterweb.debug,
+          urlInclude: [],
+          urlExclude: ["*logout*", "/admin_menu*", "*admin/*"],
+          urlDoNotFetch: ["*logout*", "*/node/*/edit", "*/node/add"],
+          elementSelector: null,
+          externalScriptObject: externalScriptObject,
+          url: {
+            "/": {
+              pageFunction: function (urlTarget, externalScriptObject) {},
+            },
+            all: {
+              pageFunction: function (urlTarget, externalScriptObject) {
+                console.log("All url callback: " + window.location.pathname);
 
-          if (window.ga) {
-            window.ga("set", "dimension1", "faster");
-            window.ga("send", "pageview", urlTarget);
-          }
+                if (window.ga) {
+                  window.ga("set", "dimension1", "faster");
+                  window.ga("send", "pageview", urlTarget);
+                }
 
-          var Drupal = externalScriptObject.Drupal;
-          if (Drupal) Drupal.attachBehaviors();
-        },
-      },
+                var Drupal = externalScriptObject.Drupal;
+                if (Drupal) Drupal.attachBehaviors();
+              },
+            },
+          },
+        };
+
+        window.addEventListener("load", function () {
+          window.faster(websiteConfig);
+        });
+      }
     },
   };
-
-  window.addEventListener("load", function () {
-    window.faster(websiteConfig);
-  });
 })(jQuery, Drupal);
